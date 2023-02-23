efcb66bd355e0c9e4fb9ace3c470540461dc615b
Mekael Turner <mistnpeace@gmail.com>
2023-02-23 18:43:16 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Mekael Turner - Developer
+
+I'm an indie programmer with a passion for fundamentals, creativity, cooperation, application, and results.
+
+🐦 Twitter:  https://www.twitter.com/mekealturner
+📖 Github:   https://www.github.com/mekkamagnus
+🗺️  Blog:     https://mekkamagnus.github.io
+
+`);
